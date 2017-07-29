f4db486c712a438e4f6a71bc972cd33bad64898a
avish cheetaram <avishcheetaram.ai@gmail.com>
2017-07-29 19:14:52 +0400

    Pointers in Cpp

---
+++ Pointer.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+//Pointer
+
+void update(int *a, int *b) {
+  int Y = *a + *b;
+  int X = abs(*a + *b);
+  *a = Y;
+  *b = X;
+
+  }
+
+  int main()
+  {
+    int a, b;
+   int *pa = &a, *pb = &b;
+
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+    
+    return 0;
+  }
